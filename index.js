4b0c7f94068b4008ebd85ad8019e0df4c727da82
ZFauzan <76593777+ZFauzan@users.noreply.github.com>
2023-11-07 21:56:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+var randomNum1 = Math.floor((Math.random() * 6) + 1);
+var randomNum2 = Math.floor((Math.random() * 6) + 1);
+var image1Changed = false;
+
+function changeImage1() {
+    diceAnimation(".img1");
+    document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNum1 + ".png");
+    document.querySelector(".dice1 p").innerHTML = "Player 1 rolled " + randomNum1;
+    image1Changed = true;
+}
+
+function changeImage2() {
+    if (!image1Changed) {
+        return; // exits this function. ensures player 1 goes first
+    }
+    diceAnimation(".img2");
+    document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNum2 + ".png");
+    document.querySelector(".dice2 p").innerHTML = "Player 2 rolled " + randomNum2;
+    changeHeading();
+}
+
+function diceAnimation(selector) {
+    document.querySelector(selector).classList.add("clicked");
+    setTimeout(function() {
+        document.querySelector(selector).classList.remove("clicked");
+    }, 100);
+}
+
+function changeHeading() {
+    document.querySelector("h1").style.color = "#F5E8C7";
+    if (randomNum1 > randomNum2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins! 🏆";
+        var win = new Audio("sounds/win.mp3");
+        win.play(); 
+    }
+    else if (randomNum1 < randomNum2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🏆";
+        win = new Audio("sounds/win.mp3");
+        win.play(); 
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw Game";
+        var draw = new Audio("sounds/draw.mp3");
+        draw.play(); 
+    }
+}
